ee7690c0884811c4f0189689b8a0e17f9603e73e
dragynir <d.korostelev@g.nsu.ru>
2022-11-14 18:24:37 +0700

    Скрипт для сегментации

--- notebooks/run_segmentation.py
+++ notebooks/run_segmentation.py
@@ -11,6 +11,52 @@ import patchify as patch
 import cv2
 
 
+def visualize(recon):
+    plt.figure(figsize=(25, 25))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(recon[256, :, :], cmap='gray')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(recon[:, 612, :], cmap='gray')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(recon[:, :, 612], cmap='gray')
+
+    plt.savefig('segmentation.png')
+
+
+def predict_volume(model, data):
+    ids_zero = np.where(data == 0)
+
+    patch_data = patch.patchify(data, (256, 256, 256), 128)
+
+    # data patches segmentation
+    batch_size = 1
+
+    if (patch_data.max() > 1):
+        patch_data = patch_data / patch_data.max()
+
+    N = patch_data.shape[0] * patch_data.shape[1] * patch_data.shape[2]
+    patch_data_merged = patch_data.reshape((N, patch_data.shape[3], patch_data.shape[4], patch_data.shape[5]))
+
+    vol = patch_data_merged
+    vol = np.reshape(vol, vol.shape + (1,))
+    result = model.predict(vol, verbose=1, batch_size=batch_size)
+    segmented = result.reshape(patch_data.shape)
+
+    recon = recon_3D(data_patches=segmented, patch_step=(128, 128, 128), patch_size=(256, 256, 256),
+                     recon_shape=data.shape)
+
+    recon = recon.astype(np.float32)
+
+    # threshold
+    recon[recon >= 0.8] = 1
+    recon[recon < 0.8] = 0
+
+    visualize(recon)
+
+
 if __name__ == '__main__':
 
     # check availible GPUS and set memory growth
@@ -29,7 +75,9 @@ if __name__ == '__main__':
     model = UNet_3D(input_size=(256, 256, 256, 1))
     model.load_weights("../models/UNet_3D_16_256.hdf5")
 
-    vol = np.ones((1, 256, 256, 256))
+    predict_volume(model, np.ones((512, 512, 512)))
 
-    result = model.predict(vol, verbose=1, batch_size=batch_size)
-    print(result.shape)
+    # debug
+    # vol = np.ones((1, 256, 256, 256))
+    # result = model.predict(vol, verbose=1, batch_size=batch_size)
+    # print(result.shape)
